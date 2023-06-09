653e286a01351cd09ae2c6dc71325cde15e1a06d
PerfectSkillsSophia <tech@perfectskills.in>
2023-06-09 21:18:02 +0530

    Database is added

--- sophia/settings.py
+++ sophia/settings.py
@@ -77,13 +77,22 @@ WSGI_APPLICATION = 'sophia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'sophia-do-user-13682473-0.b.db.ondigitalocean.com',
+        'NAME': 'defaultdb',
+        'USER': 'doadmin',
+        'PASSWORD': 'AVNS_HTXbueb0tplFrLo9Ei9',
+        'PORT': '25060',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
